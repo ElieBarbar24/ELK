99832b0c564904735b2672196708979253e999d3
ElieBarbar24 <elie24barbar@gmail.com>
2026-02-27 06:18:45 +0200

    keys Generator

--- certs-scripts-host.sh
+++ certs-scripts-host.sh
@@ -2,3 +2,4 @@ docker compose -f docker-compose.yml up --abort-on-container-exit
 sudo chown -R 1000:1000 certs
 sudo chmod -R 750 certs
 
+docker exec -it kibana /usr/share/kibana/bin/kibana-encryption-keys generate
